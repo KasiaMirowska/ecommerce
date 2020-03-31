36a637a2a7567eca0c44e0afc4595ac3329a7ab1
Kasia <kasia@miroartdesign.com>
2020-03-31 14:45:16 -0400

    header component

--- src/App.js
+++ src/App.js
@@ -4,12 +4,15 @@ import HomePage from './pages/homepage/HomePage';
 import {Route, Switch} from 'react-router-dom';
 import HatsPage from './pages/hatspage/HatsPage';
 import ShopPage from './pages/shop/ShopPage';
+import Header from './components/Header/Header';
 
 
 function App() {
   return (
     <div>
+      <Header />
       <Switch>
+     
       <Route exact path='/' component={HomePage} />
       <Route exact path='/shop' component={ShopPage} />
       </Switch>
