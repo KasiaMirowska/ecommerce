c4669ff955ada42c0cfd4d7d30ee19c26bf52d28
Kasia <kasia@miroartdesign.com>
2020-04-10 17:04:33 -0400

    dropdown cart items display

--- src/components/CartDropdown/Cart.js
+++ src/components/CartDropdown/Cart.js
@@ -1,14 +1,27 @@
 import React from 'react';
 import CustomButton from '../CustomButton/CustomButton';
+import CartItem from '../CartItem/CartItem';
 import './Cart.styles.scss';
+import { connect } from 'react-redux';
 
-const Cart = () => (
-    <div className='cart-dropdown'>
-        <div className='cart-items'>
-        <CustomButton>GO TO CHECKOUT</CustomButton>
+const mapStateToProps = (state) => ({
+    cartItems: state.cart.cartItems,
+});
+
+const Cart = (props) => {
+    const cartItems = props.cartItems.map(item => (
+        <CartItem key={item.id} {...item} />
+    ));
+
+    return (
+        <div className='cart-dropdown'>
+            <div className='cart-items'>
+                {cartItems}
+                <CustomButton>GO TO CHECKOUT</CustomButton>
+            </div> 
         </div>
+    );
 
-    </div>
-)
+};
 
-export default Cart;
+export default connect(mapStateToProps)(Cart);
---
+++ src/components/CartItem/CartItem.js
@@ -0,0 +1,20 @@
+import React from 'react';
+import './CartItem.styles.scss';
+
+
+const CartItem = (props) => {
+    const {imageUrl, price, name, quantity} = props;
+    
+    return (
+        <div className='cart-item'>
+            <img src={imageUrl} alt='item'/>
+            <div className='item-details'>
+                <span>{name}</span>
+                <span>{quantity} x ${price}</span>
+            </div>
+        </div>
+    )
+  
+}
+
+export default CartItem;
